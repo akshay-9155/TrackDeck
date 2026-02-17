917f3559515e25b93cd2a6d8703d72932bc7d510
Akshay-Anand <dev.renosync@gmail.com>
2026-02-18 00:09:51 +0530

    added cookie-parser

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors';
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(cors({
 app.use(express.json({
     limit: "16kb"
 }));
+
+app.use(cookieParser());
 
 import authRouter from './routes/auth.route.js';
 import orderRouter from './routes/order.route.js';
